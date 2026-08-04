9f33f162ea6f9b2615a6c08fb205fbb96c451909
amiabdoal <amiabdoal@gmail.com>
2026-08-04 16:38:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/v1/chat/completions', async (req, res) => {
     const nimRequest = {
   model: nimModel,
   messages: messages,
-  temperature: temperature ?? 0.6,
+  temperature: temperature ?? 0.9,
   max_tokens: max_tokens || 9024,
   ...(top_p !== undefined && { top_p }),
   ...(frequency_penalty !== undefined && { frequency_penalty }),
